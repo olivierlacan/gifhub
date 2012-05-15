6a41c833a12d9c4ba6770036fe1a94a91ca1bd0e
Olivier Lacan <olivier.lacan@gmail.com>
2012-05-15 17:35:41 -0400

    Silly Heroku and your precompile poopies.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Gifhub
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -55,5 +55,8 @@ module Gifhub
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't initialize on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
